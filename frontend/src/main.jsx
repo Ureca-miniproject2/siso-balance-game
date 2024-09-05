81768240553d9a6e0f0d2d2f9a64f0e5c74724b3
DongHyeonWon <dhe7700@naver.com>
2024-09-05 10:09:55 +0900

    chore: tanstack query 설정 추가

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import GlobalStyle from "./styles/GlobalStyle.jsx";
 import App from "./App.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <GlobalStyle />
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <GlobalStyle />
+      <App />
+    </QueryClientProvider>
   </StrictMode>
 );
