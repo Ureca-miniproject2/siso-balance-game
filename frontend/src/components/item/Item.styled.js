debaa4f0aff3634022834bd3e328a5090e685bfd
jiyounglee <skfnxh1021@naver.com>
2024-09-07 18:37:21 +0900

    style:세부조정

--- frontend/src/components/item/Item.styled.js
+++ frontend/src/components/item/Item.styled.js
@@ -1,13 +1,15 @@
 import styled from 'styled-components';
 
 export const ItemContainer = styled.div`
+    font-family:"HSSantokki2-Regular";
     margin-top: 12.9rem;
-display: grid; /* Grid 레이아웃 사용 */
-  grid-template-columns: repeat(3, 1fr); /* 3개의 열 생성 */
+    display: grid; /* Grid 레이아웃 사용 */
+    grid-template-columns: repeat(3, 1fr); /* 3개의 열 생성 */
     gap: 3.2rem;
 `;
 
 export const ItemList = styled.div`
+    padding: 5rem;
     text-align:center;
     display: flex;
     justify-content: center;
@@ -16,11 +18,12 @@ export const ItemList = styled.div`
     font-size: 2.4rem;
     width: 38.2rem;
     height: 14.7rem;
-    border: 1px solid ${(props) => props.theme.colors.black};
+    border: 0.2rem solid ${(props) => props.theme.colors.black};
     background-color: ${(props) => props.theme.colors.primaryBlue};
 `;
 
 export const ItemList2 = styled.div`
+    padding: 5rem;
     text-align:center;
     display: flex;
     justify-content: center;
@@ -29,7 +32,7 @@ export const ItemList2 = styled.div`
     font-size: 2.4rem;
     width: 38.2rem;
     height: 14.7rem;
-    border: 1px solid ${(props) => props.theme.colors.black};
+    border: 0.2rem solid ${(props) => props.theme.colors.black};
     background-color: ${(props) => props.theme.colors.primaryBlue};
     background-color: ${(props) => props.theme.colors.primaryGreen};
 `;
