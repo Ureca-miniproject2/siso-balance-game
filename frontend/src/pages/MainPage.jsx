f0438e9acaa5b6159c9ccc42e9ca3d2ec98f63a5
jiyounglee <skfnxh1021@naver.com>
2024-09-07 18:11:47 +0900

    feat:item레이이웃저장

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,3 +1,9 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import Item from '../components/item/Item';
+
 export default function MainPage() {
-  return <div>MainPage</div>;
+  return (
+    <Item />
+  );
 }
