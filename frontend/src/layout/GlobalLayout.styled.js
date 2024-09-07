bea1e83bf6890e28bec29af303a47359f508c76a
DongHyeonWon <dhe7700@naver.com>
2024-09-07 18:34:30 +0900

    style: GlobalLayout witdh 수정

--- frontend/src/layout/GlobalLayout.styled.js
+++ frontend/src/layout/GlobalLayout.styled.js
@@ -1,10 +1,7 @@
 import styled from 'styled-components';
 
 export const GlobalLayoutContainer = styled.div`
-  /* margin: 0 auto;
-  display: flex;
-  justify-content: center; */
   max-width: 1210px;
   height: 100%;
-  background-color: ${(props) => props.theme.colors.primaryGreen};
+  width: 100%;
 `;
