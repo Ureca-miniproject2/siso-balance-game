d18228f8df898830665e197c1282f40149adca5c
DongHyeonWon <dhe7700@naver.com>
2024-09-07 15:31:52 +0900

    chore: 컴포넌트 및 페이지 폴터 생성

--- frontend/src/layout/GlobalLayout.jsx
+++ frontend/src/layout/GlobalLayout.jsx
@@ -2,20 +2,27 @@ import { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 
 export default function GlobalLayout() {
-  const [e, setE] = useState(null);
+  const [e, setE] = useState([]);
   useEffect(() => {
     const a = async () => {
-      const res = await fetch('http://localhost:5173/api/user');
+      const res = await fetch('http://localhost:5173/api/item');
       const data = await res.json();
-      console.log(data.data[0].id);
-      setE(data.data[0].id);
+      console.log(data.data);
+      setE(data.data);
     };
     a();
   }, []);
   return (
     <div>
       <Outlet />
-      <div>{e}</div>
+      {e.map((item) => {
+        return (
+          <div key={item.id}>
+            <h2>{item.firstTitle}</h2>
+            <p>{item.secondTitle}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
--- frontend/src/mocks/handler.js
+++ frontend/src/mocks/handler.js
@@ -1,7 +1,12 @@
 import { http, HttpResponse } from 'msw';
 import data from './data/data.json';
+import item from './data/item.json';
 export const handlers = [
   http.get('/api/user', () => {
     return HttpResponse.json({ data });
+  }),
+
+  http.get('/api/item', () => {
+    return HttpResponse.json({ data: item });
   }),
 ];
