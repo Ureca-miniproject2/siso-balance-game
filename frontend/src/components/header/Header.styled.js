2c0296980e9310bad2e8e9de9506216275572b8d
DongHyeonWon <dhe7700@naver.com>
2024-09-07 18:22:16 +0900

    feat: Header 추가

---
+++ frontend/src/components/header/Header.styled.js
@@ -0,0 +1,38 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.header`
+  width: 100%;
+  font-family: 'HSSantokki2-Regular';
+  font-size: 1.6rem;
+  font-weight: 400;
+  padding-bottom: 1.4rem;
+  border-bottom: 2px solid #000;
+`;
+
+export const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: end;
+`;
+
+export const HeaderTitle = styled.h1`
+  font-size: 4rem;
+`;
+
+export const HeaderNav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: end;
+  gap: 2rem;
+  @media only screen and (max-width: 600px) {
+    display: none;
+  }
+`;
+export const HeaderNavItem = styled(Link)`
+  font-size: 2.4rem;
+  font-weight: 400;
+  color: #000;
+  text-decoration: none;
+  font-family: 'HSSantokki2-Regular';
+`;
