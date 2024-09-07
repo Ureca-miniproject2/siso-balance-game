3706ee004c8fd197090fd7283ae15ebee79871d1
DongHyeonWon <dhe7700@naver.com>
2024-09-07 16:21:35 +0900

    feat: ThemeProvider 추가

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import GlobalStyle from './styles/GlobalStyle.jsx';
 import App from './App.jsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './styles/theme.js';
 
 async function enableMocking() {
   if (import.meta.env.MODE !== 'development') {
@@ -21,8 +23,10 @@ enableMocking().then(() => {
   createRoot(document.getElementById('root')).render(
     <StrictMode>
       <QueryClientProvider client={queryClient}>
-        <GlobalStyle />
-        <App />
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <App />
+        </ThemeProvider>
       </QueryClientProvider>
     </StrictMode>,
   );
---
+++ frontend/src/styles/theme.js
@@ -0,0 +1,10 @@
+export const theme = {
+  colors: {
+    gray100: '#F1F1F1',
+    gray200: '#CFCFCF',
+
+    black: '#000000',
+    primaryBlue: '#4DDDFF',
+    primaryGreen: '#8DF9BB',
+  },
+};
