d840b4b76609e144fdc24519137f6c0098f662aa
DongHyeonWon <dhe7700@naver.com>
2024-09-23 00:15:09 +0900

    feat: 댓글 좋아요 및 좋아요 취소 api 추가

--- backend/src/like/like.controller.ts
+++ backend/src/like/like.controller.ts
@@ -1,7 +1,48 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Param,
+  ParseIntPipe,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { LikeService } from './like.service';
+import { AuthGuard } from '@nestjs/passport';
+import { Like } from 'src/like/like.entity';
+import { Request } from 'express';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('like')
 export class LikeController {
   constructor(private readonly likeService: LikeService) {}
+
+  @UseGuards(AuthGuard('jwt'))
+  @Post(':comment_id')
+  @ApiOperation({ summary: '댓글 좋아요' })
+  @ApiResponse({ status: 201, description: '댓글이 이미 좋아요상태 입니다.' })
+  @ApiResponse({ status: 404, description: '유저 또는 댓글이 없습니다.' })
+  @ApiResponse({
+    status: 409,
+    description: '이미 좋아요를 눌렀어요!',
+  })
+  async likeComment(
+    @Req() req: Request,
+    @Param('comment_id') commentId: number,
+  ): Promise<Like> {
+    const kakaoId = req.user.kakaoId;
+    return this.likeService.likeComment(kakaoId, commentId);
+  }
+
+  @Delete(':comment_id')
+  @ApiOperation({ summary: '댓글 좋아요 취소' })
+  @ApiResponse({ status: 200, description: '댓글 좋아요가 이미 취소되었어요.' })
+  @ApiResponse({ status: 404, description: '댓글 좋아요가 없습니다.' })
+  async unlikeComment(
+    @Req() req: Request,
+    @Param('comment_id', ParseIntPipe) commentId: number,
+  ) {
+    const kakaoId = req.user.kakaoId;
+    return this.likeService.unlikeComment(kakaoId, commentId);
+  }
 }
