5f4a3ef6e11cc090e71f8d58f8d921149a8e0a1c
DongHyeonWon <dhe7700@naver.com>
2024-09-23 03:57:12 +0900

    feat: CommentDto 추가

--- backend/src/item/item.controller.ts
+++ backend/src/item/item.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Logger, Param, ParseIntPipe } from '@nestjs/common';
 import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
-import { Comment } from 'src/comment/comment.entity';
 import { CommentService } from 'src/comment/comment.service';
+import { CommentDto } from 'src/comment/dto/comment.dto';
 
 @Controller('item')
 @ApiTags('아이템 api')
@@ -13,7 +13,7 @@ export class ItemController {
   @ApiOperation({ summary: '아이템의 댓글들을 가져옵니다.' })
   @ApiCreatedResponse({
     description: '아이템의 댓글들을 가져옵니다.',
-    type: [Comment],
+    type: [CommentDto],
   })
   async getCommentsByItemId(
     @Param('item_id', ParseIntPipe) item_id: number,
