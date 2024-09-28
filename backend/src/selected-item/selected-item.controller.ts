249bb0c305c206c3011fb05830ec8108ec0a885d
DongHyeonWon <dhe7700@naver.com>
2024-09-28 13:27:16 +0900

    chore: swagger title 및 설정 수정 (#13)

--- backend/src/selected-item/selected-item.controller.ts
+++ backend/src/selected-item/selected-item.controller.ts
@@ -2,13 +2,23 @@ import { Controller, Param, Post, Req, UseGuards } from '@nestjs/common';
 import { SelectedItemService } from './selected-item.service';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
+import {
+  ApiCookieAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
 @Controller('games/:gameId/items/:itemId/select')
+@ApiTags('아이템 선택 api')
 export class SelectedItemController {
   constructor(private readonly selectedItemService: SelectedItemService) {}
 
   @UseGuards(AuthGuard('jwt'))
   @Post()
+  @ApiCookieAuth('accessToken')
+  @ApiOperation({ summary: '아이템 선택' })
+  @ApiResponse({ status: 200, description: '정상' })
   async selectOrToggleItem(
     @Param('gameId') gameId: string,
     @Param('itemId') itemId: string,
