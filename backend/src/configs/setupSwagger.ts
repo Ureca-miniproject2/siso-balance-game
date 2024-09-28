3299d75fa5192c6c215be0ea35b8a1058a913f75
DongHyeonWon <dhe7700@naver.com>
2024-09-28 13:24:15 +0900

    chore: swagger title 및 설정 수정

--- backend/src/configs/setupSwagger.ts
+++ backend/src/configs/setupSwagger.ts
@@ -3,10 +3,9 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 export const setupSwagger = (app: INestApplication): void => {
   const options = new DocumentBuilder()
-    .setTitle('Nestjs API')
-    .setDescription('The Nestjs API description')
-    .setVersion('1.0')
-    .addTag('nestjs')
+    .setTitle('Siso-balance-game')
+    .setDescription('Siso-balance-game의 api 문서입니다.')
+    .setVersion('1.0.0')
     .addCookieAuth('accessToken') // 이 부분이 핵심입니다
     .build();
   const document = SwaggerModule.createDocument(app, options);
