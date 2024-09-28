97d4e2180c46d9ce36e10e4e7a375062eda20178
DongHyeonWon <dhe7700@naver.com>
2024-09-28 17:55:21 +0900

    fix: timezone 수정

--- backend/src/configs/typeorm.config.ts
+++ backend/src/configs/typeorm.config.ts
@@ -11,6 +11,7 @@ export class typeORMConfig implements TypeOrmOptionsFactory {
       username: process.env.TYPEORM_USERNAME,
       password: process.env.TYPEORM_PASSWORD,
       database: process.env.TYPEORM_DATABASE,
+      timezone: 'Asia/Seoul',
       entities: [__dirname + '/../**/*.entity.{js,ts}'],
       synchronize: true,
     };
