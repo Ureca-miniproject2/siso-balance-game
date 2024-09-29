1e60b8e0cb3309cff6719ee1d67ca7c488704e3b
DongHyeonWon <dhe7700@naver.com>
2024-09-30 04:54:46 +0900

    feat: 댓글 작성시 비로그인 시 로그인 페이지로 이동

--- frontend/src/components/commentInput/CommentInput.jsx
+++ frontend/src/components/commentInput/CommentInput.jsx
@@ -1,18 +1,28 @@
 import * as S from './CommentInput.styled';
 import ArrowIcon from '../common/icon/ArrowIcon';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import useCreateComment from '../../hooks/queries/useCreateComment';
+import { UserInfoContext } from '../../context/UserInfoContext';
+import { useNavigate } from 'react-router-dom';
 
 export default function CommentInput(props) {
   const { itemId } = props;
+  const navigate = useNavigate();
   const [inputValue, setInputValue] = useState('');
-
+  const { userInfo } = useContext(UserInfoContext);
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
   };
 
   const { mutate } = useCreateComment(itemId);
   const handleSubmit = () => {
+    if (inputValue === '') {
+      return;
+    }
+    if (userInfo === null) {
+      navigate('/login');
+      return;
+    }
     if (inputValue) {
       mutate({ comment_text: inputValue });
       setInputValue('');
