1a2545462ffa98f9b28160c71d8df8cfb540f5c6
DongHyeonWon <dhe7700@naver.com>
2024-09-29 21:42:48 +0900

    feat: 댓글 작성자와 사용자가 같을 시 삭제 버튼

--- frontend/src/components/comments/Comments.jsx
+++ frontend/src/components/comments/Comments.jsx
@@ -1,11 +1,13 @@
-import { useEffect, useRef } from 'react';
+import { useContext, useEffect, useRef } from 'react';
 import Comment from '../comments/comment/Comment';
 import useGetBestComments from '../../hooks/queries/useGetBestComments';
 import useGetComments from '../../hooks/queries/useGetComments';
 import useIntersectionObserver from '../../hooks/useIntersectionObserver';
 import * as S from './Comments.styled';
+import { UserInfoContext } from '../../context/UserInfoContext';
 export default function Comments(props) {
   const { bestButtonColor, itemId } = props;
+  const { userInfo } = useContext(UserInfoContext);
   const { data: bestComments } = useGetBestComments(itemId);
   const {
     data: commentsPageData,
@@ -27,7 +29,7 @@ export default function Comments(props) {
         <Comment
           key={comment.comment_id}
           isBest={true}
-          isTrash={true}
+          isTrash={userInfo?.user_id === comment.user.user_id ? true : false}
           isHeart={comment.isLikedByUser}
           bestButtonColor={bestButtonColor}
           id={comment.comment_id}
@@ -42,7 +44,7 @@ export default function Comments(props) {
           <Comment
             key={comment.comment_id}
             isBest={false}
-            isTrash={false}
+            isTrash={userInfo?.user_id === comment.user.user_id ? true : false}
             isHeart={comment.isLikedByUser}
             bestButtonColor={bestButtonColor}
             id={comment.comment_id}
