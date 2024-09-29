3e6e2a93f62315e63412212c5810da4359d357dc
DongHyeonWon <dhe7700@naver.com>
2024-09-29 21:32:40 +0900

    feat: logout api 추가

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -2,7 +2,9 @@ import {
   Controller,
   Get,
   HttpCode,
+  HttpStatus,
   Logger,
+  Post,
   Req,
   Res,
   UseGuards,
@@ -33,5 +35,26 @@ export class AuthController {
     res.cookie('accessToken', accessToken, { httpOnly: true });
 
     return res.redirect(process.env.REDIRECT_URI);
+  }
+
+  @Post('logout')
+  @ApiOperation({ summary: '로그아웃' })
+  async logout(@Res() res: Response) {
+    try {
+      // 로그아웃 처리 (쿠키 삭제)
+      res.clearCookie('accessToken', {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'strict',
+      });
+
+      // 성공 메시지 반환
+      return res.status(HttpStatus.OK).json({ message: '로그아웃되었습니다.' });
+    } catch (error) {
+      // 예외 발생 시 처리
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        message: '로그아웃에 실패했습니다. 나중에 다시 시도해주세요.',
+      });
+    }
   }
 }
